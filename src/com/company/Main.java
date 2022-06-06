902eb00739e922b7e9bb96461939aefa1f620add
CameronHyden <camh.hyden@gmail.com>
2022-06-06 16:55:21 +0100

    gave price class data and printed to the console

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Price price1 = new Price(1,"GBP/USD",1.2499, 1.2596);
+        System.out.println(price1);
     }
 }
--- src/com/company/Price.java
+++ src/com/company/Price.java
@@ -6,15 +6,59 @@ import java.util.Date;
 public class Price {
     private int id;
     private String name;
-    private int bid;
-    private int ask;
+    private double bid;
+    private double ask;
     private Timestamp dateCreated;
 
-    public Price(int id, String name, int bid, int ask, Timestamp dateCreated) {
+    public Price() { this.dateCreated = new Timestamp(new Date().getTime()); }
+
+    public Price(int id, String name, double bid, double ask) {
         this.id = id;
         this.name = name;
         this.bid = bid;
         this.ask = ask;
         this.dateCreated = new Timestamp(new Date().getTime());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBid() {
+        return bid;
+    }
+
+    public void setBid(double bid) {
+        this.bid = bid;
+    }
+
+    public double getAsk() {
+        return ask;
+    }
+
+    public void setAsk(double ask) {
+        this.ask = ask;
+    }
+
+    @Override
+    public String toString() {
+        return
+                '{' + id + "," + name + '\'' +
+                "," + bid +
+                "," + ask +
+                "," + dateCreated +
+                '}';
     }
 }
